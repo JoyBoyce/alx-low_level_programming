7ea3d337c09806bb7bc4392e598561fc7948ea3e
JoyBoyce <3875582@myuwc.ac.za>
2022-10-17 21:06:28 -0700

    tak4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * followe by a new line, execpt q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ls;
+
+	for (la = 'a'; la <= 'z'; la++)
+	}
+		if (la != 'a' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
